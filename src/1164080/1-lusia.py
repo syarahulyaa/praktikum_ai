51885963d8828883f1918a6a1d2d9c310fe769a4
lusiavio <lusiaprilia24@gmail.com>
2019-04-03 22:58:01 +0700

    aabbcc audah

---
+++ src/1164080/1-lusia.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  3 20:00:01 2019
+
+@author: ASUS
+"""
+
+filename_rock = 'genres/rock/rock.00000.au'
+x_rock, sr_rock = librosa.load(filename_rock, duration=10)
---
+++ src/1164080/2-lusia.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  3 21:04:16 2019
+
+@author: ASUS
+"""
+
+display_mfcc('genres/hiphop/hiphop.00028.au')
