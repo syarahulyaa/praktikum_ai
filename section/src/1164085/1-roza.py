fa36c8c35e01ceddb2fced0af2841e960b7b9f25
rahmiroza <rahmiroza9@gmail.com>
2019-04-03 21:42:25 +0700

    Chapter 6 Roza Praktek 1-5

---
+++ section/src/1164085/1-roza.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  3 19:59:26 2019
+
+@author: ASUS
+"""
+
+filename_jazz = 'genres/jazz/jazz.00000.au'
+x_jazz, sr_jazz = librosa.load(filename_jazz, duration=10)
---
+++ src/1164085/5-roza.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  3 18:50:04 2019
+
+@author: ASUS
+"""
+
+features, labels = generate_features_and_labels()
