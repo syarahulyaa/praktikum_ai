4999fb980cc280f4b59bf0ec674a8b0c4c9ec8fe
rahmiroza <rahmiroza9@gmail.com>
2019-04-13 19:42:54 +0700

    Rahmi Roza Chapter 7 Udah Semua

---
+++ src/1164085/erorchap7roza.py
@@ -0,0 +1,5 @@
+
+  File "<ipython-input-2-dd892d606c29>", line 2, in <module>
+    random.shuffle(imgs)
+
+NameError: name 'imgs' is not define
---
+++ src/1164085/prak14-roza.py
@@ -0,0 +1,12 @@
+# In[14]:rebuild/retrain a model with the best parameters (from the search) and use all data
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=np.shape(train_input[0])))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dense(128, activation='tanh'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+print(model.summary())
